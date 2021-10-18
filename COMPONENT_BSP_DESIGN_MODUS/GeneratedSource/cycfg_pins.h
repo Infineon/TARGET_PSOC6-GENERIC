908a0d62822bb3c32e217e14696c8b005d9f8196
gitlab-runner <do-not-reply@cypress.com>
2021-10-18 16:05:46 -0500

    Upload TARGET_PSOC6-GENERIC 3.0.0.25183

--- COMPONENT_BSP_DESIGN_MODUS/GeneratedSource/cycfg_pins.h
+++ COMPONENT_BSP_DESIGN_MODUS/GeneratedSource/cycfg_pins.h
@@ -4,13 +4,14 @@
 * Description:
 * Pin configuration
 * This file was automatically generated and should not be modified.
-* Tools Package 2.2.0.2790
-* latest-v2.X 2.0.0.6211
-* personalities 3.0.0.0
-* udd 3.0.0.562
+* Tools Package 2.4.0.5721
+* mtb-pdl-cat1 3.0.0.10651
+* personalities 5.0.0.0
+* udd 3.0.0.1377
 *
 ********************************************************************************
-* Copyright 2020 Cypress Semiconductor Corporation
+* Copyright 2021 Cypress Semiconductor Corporation (an Infineon company) or
+* an affiliate of Cypress Semiconductor Corporation.
 * SPDX-License-Identifier: Apache-2.0
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
@@ -33,7 +34,7 @@
 #include "cy_gpio.h"
 #include "cycfg_routing.h"
 #if defined (CY_USING_HAL)
-	#include "cyhal_hwmgr.h"
+    #include "cyhal_hwmgr.h"
 #endif //defined (CY_USING_HAL)
 
 #if defined(__cplusplus)
@@ -48,24 +49,24 @@ extern "C" {
 #define CYBSP_SWO_DRIVEMODE CY_GPIO_DM_STRONG_IN_OFF
 #define CYBSP_SWO_INIT_DRIVESTATE 1
 #ifndef ioss_0_port_6_pin_4_HSIOM
-	#define ioss_0_port_6_pin_4_HSIOM HSIOM_SEL_GPIO
+    #define ioss_0_port_6_pin_4_HSIOM HSIOM_SEL_GPIO
 #endif
 #define CYBSP_SWO_HSIOM ioss_0_port_6_pin_4_HSIOM
 #define CYBSP_SWO_IRQ ioss_interrupts_gpio_6_IRQn
 #if defined (CY_USING_HAL)
-	#define CYBSP_SWO_HAL_PORT_PIN P6_4
+    #define CYBSP_SWO_HAL_PORT_PIN P6_4
 #endif //defined (CY_USING_HAL)
 #if defined (CY_USING_HAL)
-	#define CYBSP_SWO P6_4
+    #define CYBSP_SWO P6_4
 #endif //defined (CY_USING_HAL)
 #if defined (CY_USING_HAL)
-	#define CYBSP_SWO_HAL_IRQ CYHAL_GPIO_IRQ_NONE
+    #define CYBSP_SWO_HAL_IRQ CYHAL_GPIO_IRQ_NONE
 #endif //defined (CY_USING_HAL)
 #if defined (CY_USING_HAL)
-	#define CYBSP_SWO_HAL_DIR CYHAL_GPIO_DIR_OUTPUT 
+    #define CYBSP_SWO_HAL_DIR CYHAL_GPIO_DIR_OUTPUT 
 #endif //defined (CY_USING_HAL)
 #if defined (CY_USING_HAL)
-	#define CYBSP_SWO_HAL_DRIVEMODE CYHAL_GPIO_DRIVE_STRONG
+    #define CYBSP_SWO_HAL_DRIVEMODE CYHAL_GPIO_DRIVE_STRONG
 #endif //defined (CY_USING_HAL)
 #define CYBSP_SWDIO_ENABLED 1U
 #define CYBSP_SWDIO_PORT GPIO_PRT6
@@ -75,24 +76,24 @@ extern "C" {
 #define CYBSP_SWDIO_DRIVEMODE CY_GPIO_DM_PULLUP
 #define CYBSP_SWDIO_INIT_DRIVESTATE 1
 #ifndef ioss_0_port_6_pin_6_HSIOM
-	#define ioss_0_port_6_pin_6_HSIOM HSIOM_SEL_GPIO
+    #define ioss_0_port_6_pin_6_HSIOM HSIOM_SEL_GPIO
 #endif
 #define CYBSP_SWDIO_HSIOM ioss_0_port_6_pin_6_HSIOM
 #define CYBSP_SWDIO_IRQ ioss_interrupts_gpio_6_IRQn
 #if defined (CY_USING_HAL)
-	#define CYBSP_SWDIO_HAL_PORT_PIN P6_6
+    #define CYBSP_SWDIO_HAL_PORT_PIN P6_6
 #endif //defined (CY_USING_HAL)
 #if defined (CY_USING_HAL)
-	#define CYBSP_SWDIO P6_6
+    #define CYBSP_SWDIO P6_6
 #endif //defined (CY_USING_HAL)
 #if defined (CY_USING_HAL)
-	#define CYBSP_SWDIO_HAL_IRQ CYHAL_GPIO_IRQ_NONE
+    #define CYBSP_SWDIO_HAL_IRQ CYHAL_GPIO_IRQ_NONE
 #endif //defined (CY_USING_HAL)
 #if defined (CY_USING_HAL)
-	#define CYBSP_SWDIO_HAL_DIR CYHAL_GPIO_DIR_BIDIRECTIONAL 
+    #define CYBSP_SWDIO_HAL_DIR CYHAL_GPIO_DIR_BIDIRECTIONAL 
 #endif //defined (CY_USING_HAL)
 #if defined (CY_USING_HAL)
-	#define CYBSP_SWDIO_HAL_DRIVEMODE CYHAL_GPIO_DRIVE_PULLUP
+    #define CYBSP_SWDIO_HAL_DRIVEMODE CYHAL_GPIO_DRIVE_PULLUP
 #endif //defined (CY_USING_HAL)
 #define CYBSP_SWDCK_ENABLED 1U
 #define CYBSP_SWDCK_PORT GPIO_PRT6
@@ -102,37 +103,37 @@ extern "C" {
 #define CYBSP_SWDCK_DRIVEMODE CY_GPIO_DM_PULLDOWN
 #define CYBSP_SWDCK_INIT_DRIVESTATE 1
 #ifndef ioss_0_port_6_pin_7_HSIOM
-	#define ioss_0_port_6_pin_7_HSIOM HSIOM_SEL_GPIO
+    #define ioss_0_port_6_pin_7_HSIOM HSIOM_SEL_GPIO
 #endif
 #define CYBSP_SWDCK_HSIOM ioss_0_port_6_pin_7_HSIOM
 #define CYBSP_SWDCK_IRQ ioss_interrupts_gpio_6_IRQn
 #if defined (CY_USING_HAL)
-	#define CYBSP_SWDCK_HAL_PORT_PIN P6_7
+    #define CYBSP_SWDCK_HAL_PORT_PIN P6_7
 #endif //defined (CY_USING_HAL)
 #if defined (CY_USING_HAL)
-	#define CYBSP_SWDCK P6_7
+    #define CYBSP_SWDCK P6_7
 #endif //defined (CY_USING_HAL)
 #if defined (CY_USING_HAL)
-	#define CYBSP_SWDCK_HAL_IRQ CYHAL_GPIO_IRQ_NONE
+    #define CYBSP_SWDCK_HAL_IRQ CYHAL_GPIO_IRQ_NONE
 #endif //defined (CY_USING_HAL)
 #if defined (CY_USING_HAL)
-	#define CYBSP_SWDCK_HAL_DIR CYHAL_GPIO_DIR_BIDIRECTIONAL 
+    #define CYBSP_SWDCK_HAL_DIR CYHAL_GPIO_DIR_BIDIRECTIONAL 
 #endif //defined (CY_USING_HAL)
 #if defined (CY_USING_HAL)
-	#define CYBSP_SWDCK_HAL_DRIVEMODE CYHAL_GPIO_DRIVE_PULLDOWN
+    #define CYBSP_SWDCK_HAL_DRIVEMODE CYHAL_GPIO_DRIVE_PULLDOWN
 #endif //defined (CY_USING_HAL)
 
 extern const cy_stc_gpio_pin_config_t CYBSP_SWO_config;
 #if defined (CY_USING_HAL)
-	extern const cyhal_resource_inst_t CYBSP_SWO_obj;
+    extern const cyhal_resource_inst_t CYBSP_SWO_obj;
 #endif //defined (CY_USING_HAL)
 extern const cy_stc_gpio_pin_config_t CYBSP_SWDIO_config;
 #if defined (CY_USING_HAL)
-	extern const cyhal_resource_inst_t CYBSP_SWDIO_obj;
+    extern const cyhal_resource_inst_t CYBSP_SWDIO_obj;
 #endif //defined (CY_USING_HAL)
 extern const cy_stc_gpio_pin_config_t CYBSP_SWDCK_config;
 #if defined (CY_USING_HAL)
-	extern const cyhal_resource_inst_t CYBSP_SWDCK_obj;
+    extern const cyhal_resource_inst_t CYBSP_SWDCK_obj;
 #endif //defined (CY_USING_HAL)
 
 void init_cycfg_pins(void);
--- README.md
+++ README.md
@@ -60,7 +60,7 @@ See the [BSP API Reference Manual][api] for the complete list of the provided in
 * [PSOC6-GENERIC Documentation](https://github.com/infineon/TARGET_PSOC6-GENERIC)
 * [Cypress Semiconductor, an Infineon Technologies Company](http://www.cypress.com)
 * [Infineon GitHub](https://github.com/infineon)
-* [ModusToolbox](https://www.cypress.com/products/modustoolbox-software-environment)
+* [ModusToolbox™](https://www.cypress.com/products/modustoolbox-software-environment)
 
 [api]: https://infineon.github.io/TARGET_PSOC6-GENERIC/html/modules.html
 [settings]: https://infineon.github.io/TARGET_PSOC6-GENERIC/html/md_bsp_settings.html
